d76efd80d5b75ffc2b1aadf9bdb08800565a9205
CALYPS <fakachampionship@gmail.com>
2025-05-30 23:43:50 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -224,8 +224,8 @@ function App() {
             />
           </div>
           {/* Zone principale de chat */}
-          <div className="flex-1 flex flex-col">
-            <div className="flex-1 overflow-y-auto px-6 py-4 space-y-3">
+          <div className="flex-1 flex flex-col max-h-screen">
+            <div className="flex-1 overflow-y-auto px-2 sm:px-6 py-2 sm:py-4 space-y-2 sm:space-y-3">
               {messages.map((message, index) => (
                 <ChatMessage
                   key={index}
@@ -235,7 +235,7 @@ function App() {
               ))}
             </div>
             {/* Zone de saisie fixe en bas */}
-            <div className="border-t-2 border-red-900 bg-black px-6 py-2">
+            <div className="border-t-2 border-red-900 bg-black px-2 sm:px-6 py-1 sm:py-2 mt-auto">
               <ChatInput onSendMessage={sendMessage} onSendFile={sendFile} />
             </div>
           </div>
